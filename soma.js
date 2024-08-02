3589f68796ad8dfd7fabab2413472deb96dff8c8
dani-moura081 <dani.fe.olav.rodrigues@gmail.com>
2024-08-02 11:36:32 -0300

    api_subtracao

--- soma.js
+++ soma.js
@@ -8,8 +8,17 @@ servidor.get("/soma/:n1/:n2",(req,resp)=>{
     let n1 =Number(req.params.n1)
     let n2 =Number(req.params.n2)
     let soma = n1+n2
-    resp.send(" a soma e "+ soma)
+    resp.send(" a soma e " + soma)
 })
 
+servidor.get('/subtracao/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+    let subtracao = n1-n2
+    resp.send({ 
+        subtracao:subtracao
+    })
+        
+})
 
-servidor.listen((5001,()=>console.log("Ta rodando?")))
+servidor.listen(5050, () =>console.log("Ta rodando?") )
